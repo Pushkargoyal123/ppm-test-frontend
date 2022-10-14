0035b9fa620a6b33faa68114263c7992b50ff55d
pushkargoyal123 <pushkargoyal36@gmail.com>
2022-10-14 20:14:22 +0530

    comiting

--- src/logged_in/components/GroupDropDown.js
+++ src/logged_in/components/GroupDropDown.js
@@ -15,18 +15,20 @@ export default function GroupDropDown(props) {
         const groupList = async () => {
             const data = await getData("group/fetchALLGroupsByUserId");
             if (data) {
-                setListGroup(data.data);
-                if (data.data.length) {
-                    if(!sessionStorage.getItem("groupId")){
+                if (data.data.length && data.data[0].ppm_group.value) {
+                    setListGroup(data.data);
+                    if (!sessionStorage.getItem("groupId") || sessionStorage.getItem("groupId") === "".trim()) {
                         props.setGroupId(data.data[0].ppm_group.id)
-                    }else{
+                    } else {
                         props.setGroupId(sessionStorage.getItem("groupId"));
                     }
-                    if (props.userGroupId) props.setUserGroupId(data.data[0].id)
+                    if (props.userGroupId){
+                        props.setUserGroupId(data.data[0].id)
+                    }
                 }
             }
         }
-        if(userData){
+        if (userData) {
             groupList();
         }
         // eslint-disable-next-line
@@ -47,40 +49,43 @@ export default function GroupDropDown(props) {
     return <Grid container spacing={4}>
         <Grid item sm={4} style={{ marginLeft: 20 }}>
             {
-                listGroup[0] && listGroup.length === 1 ?
-                    <div style={{ fontSize: 20, fontWeight: "bold", textTransform: "uppercase" }}>{listGroup[0].ppm_group.name + "-" + listGroup[0].ppm_group.value}</div>
-                    :
-                    <FormControl style={{ width: 200 }}>
-                        <InputLabel htmlFor="outlined-age-native-simple">Group</InputLabel>
-                        <Select
-                            value={props.groupId}
-                            onChange={(event) => handleSelectChange(event.target.value)}
-                            label="Group"
-                            style={{ textTransform: "uppercase" }}
-                            placeholder="Group"
-                            inputProps={{
-                                name: 'Group',
-                                id: 'outlined-age-native-simple',
-                            }}
-                        >
-                            {
-                                listGroup.map(function (item) {
-                                    return <MenuItem
-                                        style={{ textTransform: "uppercase" }}
-                                        key={item.ppm_group.id}
-                                        value={item.ppm_group.id}>
-                                        {item.ppm_group.name + "-" + item.ppm_group.value}
-                                    </MenuItem>;
-                                })
-                            }
-                        </Select>
-                    </FormControl>
+                listGroup.length ?
+                    listGroup[0] && listGroup.length === 1 ?
+                        <div style={{ fontSize: 20, fontWeight: "bold", textTransform: "uppercase" }}>{listGroup[0].ppm_group.name + "-" + listGroup[0].ppm_group.value}</div>
+                        :
+                        <FormControl style={{ width: 200 }}>
+                            <InputLabel htmlFor="outlined-age-native-simple">Group</InputLabel>
+                            <Select
+                                value={props.groupId}
+                                onChange={(event) => handleSelectChange(event.target.value)}
+                                label="Group"
+                                style={{ textTransform: "uppercase" }}
+                                placeholder="Group"
+                                inputProps={{
+                                    name: 'Group',
+                                    id: 'outlined-age-native-simple',
+                                }}
+                            >
+                                {
+                                    listGroup.map(function (item) {
+                                        return <MenuItem
+                                            style={{ textTransform: "uppercase" }}
+                                            key={item.ppm_group.id}
+                                            value={item.ppm_group.id}>
+                                            {item.ppm_group.name + "-" + item.ppm_group.value}
+                                        </MenuItem>;
+                                    })
+                                }
+                            </Select>
+                        </FormControl>
+                 :
+            <div></div>
             }
         </Grid>
         <Grid item sm={4} style={{ fontSize: 40, textAlign: "center" }}>
             {
                 props.heading ? <u>{props.heading}</u> :
-                    <span style={{fontSize: 20}}>{props.current}</span>
+                    <span style={{ fontSize: 20 }}>{props.current}</span>
             }
         </Grid>
 
